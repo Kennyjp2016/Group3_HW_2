27be243f92eb9c7de8d35b9628129d6cd5d52ac0
Kennyjp2016 <43482403+Kennyjp2016@users.noreply.github.com>
2018-10-17 00:25:25 -0400

    Update SumOfNum.cpp
    Complete Code Requires part B write up

--- SumOfNum.cpp
+++ SumOfNum.cpp
@@ -8,6 +8,15 @@
 ** in a variable that can hold it, like a double or long
 */
 
-void sumofNum(){
-	//TODO
+void sumofNum()
+{
+	double total_numbers, count, total_sum;
+    cout << "This program numbers sequentially together starting at 1"<< "\nEnter the largest number number:" << endl; 
+	cin >> total_numbers; // collects total numbers of inputs
+	count = 0;
+	total_sum = 0;
+	while (count < total_numbers) {
+		total_sum = ++count + total_sum; // Adds Total Sum
+	}
+	cout << "The total sum is " << total_sum << endl;
 }
