51f8f6f35bb026879e3504008eb0121c13d12303
Kennyjp2016 <43482403+Kennyjp2016@users.noreply.github.com>
2018-10-17 01:01:08 -0400

    Completed part A
    Difficulty getting it to print with correct decimal places consistantly

--- MemFee.cpp
+++ MemFee.cpp
@@ -7,10 +7,27 @@
 **
 ** All monetary values are in dollars.
 */
-
 const int YEARLY_FEE = 2600;
 const float YEARLY_CHANGE = 0.04;
+const int YEARS = 6;
 
 void memFee () {
-	//TODO
+{
+	setprecision(2);
+
+	int count;
+	double membership_fee = YEARLY_FEE;
+	count = 1;
+	cout << "This program calculates the projected membership costs using the estimated cost increases" << endl;
+	cout << "Year " << count << " membership fees will be $" << membership_fee << endl;
+	while (count < YEARS)
+	{
+		++count;
+		membership_fee = (membership_fee * YEARLY_CHANGE) + membership_fee;
+		cout << "Year " << count << " membership fees will be $" << membership_fee << endl;
+	}
+
+}
+void memFeeModified () {
+	
 }
